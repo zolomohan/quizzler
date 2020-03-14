6b26f145b4c69699baf12755daaecb466c6574a5
zolomohan <mohanzolomon@gmail.com>
2020-03-15 02:17:18 +0530

    Init Commit

---
+++ lib/QuizBrain.dart
@@ -0,0 +1,79 @@
+import 'Question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+
+  List<Question> _questionBank = [
+    Question(
+      question: 'Some cats are actually allergic to humans',
+      answer: true,
+    ),
+    Question(
+      question: 'You can lead a cow down stairs but not up stairs.',
+      answer: false,
+    ),
+    Question(
+      question: 'Approximately one quarter of human bones are in the feet.',
+      answer: true,
+    ),
+    Question(
+      question: 'A slug\'s blood is green.',
+      answer: true,
+    ),
+    Question(
+      question: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+      answer: true,
+    ),
+    Question(
+      question: 'It is illegal to pee in the Ocean in Portugal.',
+      answer: true,
+    ),
+    Question(
+      question:
+          'No piece of square dry paper can be folded in half more than 7 times.',
+      answer: false,
+    ),
+    Question(
+      question:
+          'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+      answer: true,
+    ),
+    Question(
+      question:
+          'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+      answer: false,
+    ),
+    Question(
+      question:
+          'The total surface area of two human lungs is approximately 70 square metres.',
+      answer: true,
+    ),
+    Question(
+      question: 'Google was originally called \"Backrub\".',
+      answer: true,
+    ),
+    Question(
+      question:
+          'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+      answer: true,
+    ),
+    Question(
+      question:
+          'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+      answer: true,
+    ),
+  ];
+
+  String getQuestion() => _questionBank[_questionNumber].question;
+
+  int getNumberOfQuestions() => _questionBank.length;
+
+  bool isAnswer(bool userAnswer) => _questionBank[_questionNumber].answer == userAnswer;
+
+  void nextQuestion() =>
+      _questionNumber < _questionBank.length - 1 ? _questionNumber++ : null;
+
+  bool isFinalQuestion() => _questionNumber == _questionBank.length - 1;
+
+  void reset() => _questionNumber = 0;
+}
---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:quizzler/QuizBrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() => runApp(Quizzler());
+QuizBrain quizBrain = QuizBrain();
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  int score = 0;
+  List<Icon> scoreKeeper = [];
+
+  var alertStyle = AlertStyle(
+      isCloseButton: false,
+      isOverlayTapDismiss: false,
+      backgroundColor: Colors.blueGrey[500]);
+
+  void onAnswer(BuildContext context, bool userAnswer) {
+    checkAnswer(userAnswer);
+    quizBrain.nextQuestion();
+    if (quizBrain.isFinalQuestion()) {
+      Alert(
+              context: context,
+              title: 'Complete',
+              desc: 'Your Score: $score/${quizBrain.getNumberOfQuestions()}',
+              style: alertStyle)
+          .show();
+      reset();
+    }
+  }
+
+  void reset() {
+    quizBrain.reset();
+    score = 0;
+    scoreKeeper = [];
+  }
+
+  void checkAnswer(bool userAnswer) {
+    quizBrain.isAnswer(userAnswer)
+        ? setState(() {
+            score++;
+            scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+          })
+        : setState(() => scoreKeeper.add(Icon(Icons.close, color: Colors.red)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(0.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestion(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+                textColor: Colors.white,
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () => onAnswer(context, true)),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () => onAnswer(context, false),
+            ),
+          ),
+        ),
+        Row(children: scoreKeeper)
+      ],
+    );
+  }
+}
